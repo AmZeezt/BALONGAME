97887769f0838db2c9d09b9d24348671c47a281b
AmZeezt <101056334+AmZeezt@users.noreply.github.com>
2023-06-11 19:26:41 +0200

    RenderWindow
    - Rendering window
    - Close functionality
    - Frame rendering

--- main.cpp
+++ main.cpp
@@ -1,7 +1,47 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Network.hpp>
+
+using namespace sf;
 
 int main() {
 
+	//Window declaration
+	RenderWindow window(VideoMode(800, 600), "BALONG", Style::Titlebar | Style::Close);
+	Event ev;
+
+	//Game loop
+	while (window.isOpen())
+	{
+		//App status listener
+		while (window.pollEvent(ev))
+		{
+			switch (ev.type) {
+			//Close event
+			case Event::Closed:
+				window.close();
+				break;
+			//Key Close events
+			case Event::KeyPressed:
+				if (ev.key.code == Keyboard::Escape)
+					window.close();
+			}
+		}
+
+		//App updater
+
+		//App renderer
+		window.clear(); //Scrap frame
+
+		//Draw
+
+		window.display(); //Render frame
+	}
+
+
+	//Proper close
 	return 0;
 }
