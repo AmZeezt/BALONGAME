fa3be113d2ab7989c91868ba98af123358004fe6
Jonkis Mikołaj <101056334+AmZeezt@users.noreply.github.com>
2023-06-13 20:15:43 +0200

    Emergency commit

--- Terrain.h
+++ Terrain.h
@@ -1,10 +1,27 @@
 #pragma once
+
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Network.hpp>
+
+using namespace sf;
+
 class Terrain
 {
 private:
+	RectangleShape shape;
 
+	void initVariables();
+	void initShape(const RenderWindow& window);
 public:
-	Terrain();
+	Terrain(const RenderWindow& window);
 	virtual ~Terrain();
+
+	//Functions
+	const RectangleShape& getShape() const;
+	void update();
+	void render(RenderTarget& target);
 };
 
